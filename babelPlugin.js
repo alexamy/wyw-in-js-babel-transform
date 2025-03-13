b2dae635d52a7d3f2b91f15ebd846bd13b9b6c9e
Aleksandr Chashchin <ac@happynumbers.com>
2025-03-13 12:44:12 +0100

    add babel plugin and tsx

---
+++ babelPlugin.js
@@ -0,0 +1,8 @@
+export default function() {
+  console.log("In babel plugin!");
+
+  return {
+    name: 'babel-plugin-example',
+    visitor: {},
+  };
+}
--- build.js
+++ build.js
@@ -1,8 +1,23 @@
 import { build } from "esbuild";
+import babelPlugin from "esbuild-plugin-babel";
 
 build({
+  logLevel: 'info',
   entryPoints: ["src/index.js"],
   outfile: "dist/index.js",
+  plugins: [
+    babel(),
+  ],
 });
 
-console.log("Build completed.");
+function babel() {
+  return babelPlugin({
+    filter: /\.js$/,
+    config: {
+      cwd: process.cwd(),
+      plugins: [
+        './babelPlugin.js'
+      ],
+    },
+  });
+}
